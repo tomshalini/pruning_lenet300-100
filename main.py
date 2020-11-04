f1db2aea316f63a9660e357c3a7f20d90ddf9e03
shalini <shalini.shalini@itwm.fhg.de>
2020-11-04 16:11:55 +0100

    added mask for iterative pruning

--- main.py
+++ main.py
@@ -4,24 +4,28 @@ from tensorflow.keras import models, layers
 
 from copy import deepcopy
 from dataset import mnist_dataset
-from model import LeNet_300_100, train
+from model import LeNet_300_100, train, retrain
 from util import get_plot, print_nonzeros
 from prune_model import prune_weights
 
 batch_size = 128
 lr=0.001
-num_epochs = 30
+num_epochs = 20
 img_rows, img_cols = 28, 28
 input_size= 784
 num_classes=10
 
-x_train, y_train, x_test, y_test=mnist_dataset(num_classes) # load preprocessed MNIST dataset
+# load preprocessed MNIST dataset
+x_train, y_train, x_test, y_test=mnist_dataset(num_classes) 
 
-model = LeNet_300_100(input_size, num_classes) # build model
+# build model
+model = LeNet_300_100(input_size, num_classes) 
 opt = keras.optimizers.Adam(learning_rate=lr)
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) 
-model_path="lenet300-100.h5"               #path to save original model
-model,history=train(model,x_train, y_train,batch_size,num_epochs,model_path) #training
+
+#path to save original model
+model_path="lenet300-100.h5"               
+model,history=train(model,x_train, y_train,batch_size,num_epochs,model_path) #training 
 original_loss,original_accuracy = model.evaluate(x_test, y_test, verbose=0)  #testing
 print("Result of Original Model:")
 print('Original Model loss:', original_loss)
@@ -32,9 +36,11 @@ get_plot(history)
 #Iterative training (prune+retrain) in single iteration and we run until we get pruning accuracy >= original model accuracy.
 #Threshold is calculated based on standard deviation of weights and sensitivity/quality factor.
 
-for i in range(1,100):
+for i in range(1,10):
   pretrained_model= keras.models.load_model(model_path)  
   quality_parameter = 1.6      #sensitivity factor to calculate threshold
+
+  #Pruning
   for layerid in range(len(pretrained_model.layers)):
     layer=pretrained_model.layers[layerid]
     weight=layer.get_weights()
@@ -43,25 +49,31 @@ for i in range(1,100):
             updated_weights = prune_weights(temp_weight[0],quality_parameter) #function call to prune weight based on threshold
             temp_weight[0]= updated_weights
             layer.set_weights(temp_weight)   #set layers weights with pruned weight
-  pretrained_model.save("pruned_lenet_300-100.h5") #save pruned model
+  #save pruned model
+  pretrained_model.save("pruned_lenet_300-100.h5") 
   pruned_loss,pruned_accuracy = pretrained_model.evaluate(x_test, y_test, verbose=0) # test pruned model
-  if pruned_accuracy >=original_accuracy:   # iterative retraining exit condition
+
+  # iterative retraining exit condition
+  if pruned_accuracy >=original_accuracy:   
     print(f'Final Result:')
     print_nonzeros(pretrained_model)
     print('Final Loss:', pruned_loss)
     print('Final Accuracy:',pruned_accuracy)
     pretrained_model.save("final_model_pruned.h5")
     exit(0)
+  
   print(f'Result of {i} Iteration:')
   print_nonzeros(pretrained_model)   #function to measure pruned weights and compression rate
   print('Loss after Pruning:', pruned_loss)
   print('Accuracy after Pruning:',pruned_accuracy)
-    
+   
   pruned_model=  keras.models.load_model("pruned_lenet_300-100.h5",compile=False) #load pruned model
   opt = keras.optimizers.Adam(learning_rate=lr/10)  # update optimizer with lr=lr/10
   pruned_model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
   retrain_model_path="retrain_lenet.h5"
-  retrain_model,history=train(pruned_model,x_train, y_train,batch_size,10,retrain_model_path) #retrain model with 10 epochs to learn important weights those are removed  during pruning
+  
+  #retrain model with 10 epochs to learn important weights those are removed  during pruning
+  retrain_model,loss_value = retrain(pruned_model,opt,x_train, y_train,batch_size,10,retrain_model_path) 
   retrain_loss,retrain_accuracy  = retrain_model.evaluate(x_test, y_test, verbose=0)
 
   print_nonzeros(retrain_model) 
--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+ import numpy as np
+from copy import deepcopy
 import tensorflow as tf
 import tensorflow.keras as keras
 
@@ -14,4 +16,49 @@ def train(model,x_train, y_train,batch_size,num_epochs,model_path):
                     verbose=1)
   model.save(model_path)
   return model,history
+
+def retrain(model,opt,x_train, y_train,batch_size,num_epochs,model_path):
+  
+  train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+  train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
+  
+  loss_fn= tf.keras.losses.BinaryCrossentropy()
+
+  
+  for epoch in range(num_epochs):
+    print("\nStart of epoch %d" % (epoch,))
+
+    # Iterate over the batches of the dataset.
+    for batch_id, (x_train_batch, y_train_batch) in enumerate(train_dataset):
+
+        with tf.GradientTape() as tape:
+
+            logits = model(x_train_batch, training=True) 
+            loss_value = loss_fn(y_train_batch, logits)
+
+        grads = tape.gradient(loss_value, model.trainable_weights)
+        updated_grads=[]
+        for var, g in zip(model.trainable_variables, grads):
+            if 'kernel' in var.name:
+                weight_copy = deepcopy(var.numpy())
+                mask = tf.constant(np.where(weight_copy == 0, 0, 1),dtype=np.float32)
+                updated_grads.append(tf.multiply(g,mask))
+            else:
+                updated_grads.append(g)
+                
+
+        # Run one step of gradient descent by updating
+        # the value of the variables to minimize the loss.
+        
+        opt.apply_gradients(zip(updated_grads, model.trainable_weights))
+
+        # Log every 200 batches.
+        if batch_id % 200 == 0:
+            print(
+                "Training loss (for one batch) at step %d: %.4f"
+                % (batch_id, float(loss_value))
+            )
+            print("Seen so far: %s samples" % ((batch_id + 1) * batch_size))
+  model.save(model_path)
+  return model, loss_value
 
